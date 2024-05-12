69a29a581d6fef6314ac66ca58bed45d0fc13078
Sole Garcia <msgarciavago@gmail.com>
2024-05-12 18:10:51 +0000

    functions for game and colors for results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,51 @@
-// rock paper scissors functionality
+// Const for the rock paper scissors game
 
 const choices = ["rock", "paper", "scissors"];
-const playerDisplay = dco
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+// Functions playGame for computer and player choices
+
+function playGame(playerChoice) {
+
+    // Computer random choice
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if (playerChoice === computerChoice) {
+        result = "IT'S A TIE!";
+    }
+    else {
+        switch (playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+        }
+    }
+
+    // Player and computer choices displayed on screen
+    playerDisplay.textContent = `You: ${playerChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}`;
+
+    // Result of playGame displayed on screen
+    resultDisplay.textContent = result;
+
+    // Colors for results
+    switch (result) {
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            break;
+
+    }
+
+}
